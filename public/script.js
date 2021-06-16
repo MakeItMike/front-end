6880a04a8c2ccdac282e530addf6a226d56e30cb
Michael Woodyard <mbwoodyard@hotmail.com>
2021-06-16 15:14:41 -0400

    Style overhaul. Images added. Colors changed. Page 1 added. Page 2 added. Page 3 added.

--- public/script.js
+++ public/script.js
@@ -14,7 +14,14 @@
 // SECTION Functions                     //
 //———————————————————————————————————————//
 
-
+function bars() {
+    var x = document.getElementById("nav-element");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 //———————————————————————————————————————//
 // SECTION Sockets                       //
